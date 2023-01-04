1fde91a10344285a81a882a5f940ed194811c262
Ahmad-045 <ahmad.amin@devsinc.com>
2023-01-04 15:13:24 +0500

    Add/Remove task + can change reminder for task

---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <Task @toggle-remainder="$emit('toggle-remainder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task';
+
+export default {
+  name: 'Tasks',
+  components: { Task },
+  props: {
+    tasks: Array
+  },
+  emits: ['delete-task', 'toggle-remainder']
+}
+</script>
